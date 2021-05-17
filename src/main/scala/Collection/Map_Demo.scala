fa25e1554fc593c4874ed98faa6928fda87e5027
Wente Jing <wjing@savi.com>
2021-05-17 11:51:49 -0400

    adding more methods

--- src/main/scala/Collection/Map_Demo.scala
+++ src/main/scala/Collection/Map_Demo.scala
@@ -1,9 +1,16 @@
 package Collection
 
-object Map_Demo {
+object Map_Demo extends App {
   val myMap: Map[Int, String] = Map(801 -> "Max", 802 -> "Tom", 804 -> "June")
   val myMap_2: Map[Int, String]= Map(805 -> "Jeremy")
-  def main(args: Array[String]): Unit = {
+
+  val romanNumerals = Map("I" -> 1, "V" -> 5, "x" -> 10)
+  val capitalOfCountry = Map("US" -> "Washington", "Switzerland" -> "Bern")
+
+  println(
+    capitalOfCountry("US"),
+    capitalOfCountry("China")
+  )
     println(myMap)
     println(myMap(801))
     println(myMap.keys)
@@ -17,5 +24,5 @@ object Map_Demo {
 
     println(myMap.contains(801))
     println(myMap ++ myMap_2)
-  }
+
 }
--- src/main/scala/Collection/NQueens.scala
+++ src/main/scala/Collection/NQueens.scala
@@ -2,7 +2,7 @@ package Collection
 
 object NQueens extends App {
   def queens(n: Int): Set[List[Int]] = {
-    def placeQueens(k: Int): Set[List[Int]] = {
+    def placeQueens(k: Int): Set[List[Int]] =
       if (k == 0) Set(List())
       else
         for {
@@ -10,15 +10,18 @@ object NQueens extends App {
           col <- 0 until n
           if isSafe(col, queens)
         } yield col :: queens
-      placeQueens(n)
-    }
+        
+    placeQueens(n)
+  }
 
     def isSafe(col: Int, queens: List[Int]): Boolean = {
       val row = queens.length
       val queenWithRow = (row - 1 to 0 by -1) zip queens
       queenWithRow forall{
-        case (r, c) => col !=c && math.abs(col -c ) != row -r
+        case (r, c) => col !=c && math.abs(col -c) != row -r
       }
     }
-  }
+  println(
+    queens(4)
+  )
 }
